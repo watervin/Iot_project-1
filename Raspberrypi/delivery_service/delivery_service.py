baf52987cdf78b2c53b350a04af13dd0806138a8
qbae <kkyub5118@gmail.com>
2022-04-19 14:47:05 +0900

    project_day6

--- Raspberrypi/delivery_service/delivery_service.py
+++ Raspberrypi/delivery_service/delivery_service.py
@@ -19,6 +19,7 @@ import paho.mqtt.publish as publish
 
 MQTT_SERVER = "175.211.162.37"  #Write Server IP Address
 MQTT_PATH = "Image"
+MQTT_PATH1 = "Android"
 
 
 # 거리측정
@@ -28,15 +29,25 @@ sensor = DistanceSensor(23,24) # echo, trig
 nw = datetime.now()
 
 state = 0
+state_a = 0 
 
 while True:
     print(f"Distance = {sensor.distance:.2f}m")
     dis = round(sensor.distance,2)
     sleep(1)
     if (dis <= 0.2) :
+
         state = 1
+        # 택배가 도착 했을 때 한 번만 울리게 하기위하여 지정해준 변수 
+        state_a += 1 
+
+    if(state_a == 1) and (dis <= 0.2):
+        publish.single(MQTT_PATH1, "arrived_box", hostname=MQTT_SERVER)
+        print("send message")
+            
     if(state == 1) and (dis > 0.5):
         state = 0 
+        state_a = 0
         
         # Create File
         my_file = open(f'delivery_service/img{nw.year}-{nw.month}-{nw.day}_{nw.hour}:{nw.minute}:{nw.second}.jpg', 'wb')
